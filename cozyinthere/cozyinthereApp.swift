332d7f91b5de097a59ecf5b80bf73b4240587b28
jkimdev <jkim.dev@icloud.com>
2023-08-19 11:49:57 +0900

    project initialized

---
+++ cozyinthere/cozyinthereApp.swift
@@ -0,0 +1,17 @@
+//
+//  cozyinthereApp.swift
+//  cozyinthere
+//
+//  Created by Jimmy on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct cozyinthereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
