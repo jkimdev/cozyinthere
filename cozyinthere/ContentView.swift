bdf1e086dcf4149d9fe34c42cf57a92d58a7a45c
jkimdev <jkim.dev@icloud.com>
2023-08-21 18:54:16 +0900

    Layout : add main card view

--- cozyinthere/ContentView.swift
+++ cozyinthere/ContentView.swift
@@ -16,6 +16,42 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+    @ViewBuilder
+    private func card(imageName: String) -> some View {
+        VStack(alignment: .leading) {
+            Image(imageName)
+                .resizable()
+                .aspectRatio(16 / 9, contentMode: .fit)
+            VStack(alignment: .leading) {
+                HStack {
+                    Image("writer.image")
+                        .clipShape(Circle())
+                    Text("Chaeho Lim")
+                        .font(.system(size: 12))
+                }
+                HStack(alignment: .lastTextBaseline) {
+                    Text("St. Mary Lake")
+                        .font(.title3)
+                        .bold()
+                    Spacer()
+                    Text("5 min read")
+                        .font(.caption2)
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding()
+        }
+        .background()
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+        .onTapGesture {
+            withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.6)) {
+                showsDetail = true
+            }
+            withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7).delay(0.2)) {
+                showContent = true
+            }
+        }
+        .matchedGeometryEffect(id: "card", in: animation)
     }
 }
 
