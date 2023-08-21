3d181af0b518ef0a915f16ecff559203a85217d3
jkimdev <jkim.dev@icloud.com>
2023-08-21 18:56:19 +0900

    Layout : add hero animation in main view

--- cozyinthere/ContentView.swift
+++ cozyinthere/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Namespace var animation
+    @State private var isPressed: Bool = false
+    @State private var showsDetail: Bool = false
+    @State private var showContent: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView(showsIndicators: false) {
+            VStack {
+                Text("COZYINTHERE")
+                    .font(.title3)
+                    .foregroundColor(Color("solid.green"))
+                    .padding(.bottom)
+
+                VStack(alignment: .leading, spacing: 16) {
+                    dateCapsule(date: "Aug 19")
+                    card(imageName: "stmarylake")
+//                    dateCapsule()
+//                    card(imageName: "umbagog")
+//                    dateCapsule()
+//                    card(imageName: "rainbowlake")
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
         }
         .padding()
+        .background(LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.9), Color.black.opacity(0.8)]), startPoint: .bottomLeading, endPoint: .topLeading)
+        )
+        .ignoresSafeArea(edges: .bottom)
+
+        .overlay {
+            if showsDetail {
+                VStack {
+                    card(imageName: "stmarylake")
+                    Spacer()
+                }
+                .ignoresSafeArea()
+                .background()
+                .onTapGesture {
+                    withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.6)) {
+                        showsDetail = false
+                        showContent = false
+                    }
+                }
+            }
+        }
+    }
+
     @ViewBuilder
     private func dateCapsule(date: String) -> some View {
         Text(date)
@@ -26,6 +64,7 @@ struct ContentView: View {
             .background(Color.white)
             .clipShape(Capsule())
     }
+
     @ViewBuilder
     private func card(imageName: String) -> some View {
         VStack(alignment: .leading) {
